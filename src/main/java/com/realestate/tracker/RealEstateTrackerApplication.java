ed20ba80f13a3bbcf63a4469e82017223b9944a0
jykim <yong6302@gmail.com>
2025-10-28 20:40:05 +0900

    Initial project setup with Spring Boot and React

---
+++ src/main/java/com/realestate/tracker/RealEstateTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realestate/tracker/RealEstateTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
